54d51da968e066427a7ef11afbaf3113ca471243
tracey <tracey.d.huang@gmail.com>
2022-09-03 15:55:50 +1000

    Change z-index of collapse nav bar so it's displaying over hero text

--- script.js
+++ script.js
@@ -32,3 +32,56 @@ const animateFadeElements = document.querySelectorAll('.animate-fade');
 window.onload = function() {
     animateFadeElements.forEach(element => element.classList.add('fade-in'));
 }
+
+//fireflies
+const FIREFLIES = 30;
+const maxHeight = 1000;
+const maxWidth = 1600;
+const fireFlyBody = document.querySelector('.stars');
+
+const random = (min, max) => {
+    return Math.floor(Math.random() * ( max - min ) + min);
+}
+for(var i = 0; i < FIREFLIES; i++) {
+    let div = document.createElement('div');
+    div.setAttribute('class', 'firefly');
+    fireFlyBody.append(div);
+}
+var fireflies = document.querySelectorAll('.firefly');
+for(var i = 0; i < FIREFLIES; i++) {
+    let ix = random(-10, maxWidth+10);
+    let fx = random(-10, maxWidth+10);
+    
+    let iy = random(-10, maxHeight+10);
+    let fy = random(-10, maxHeight+10);
+
+    gsap.fromTo(fireflies[i], {
+        x: ix,
+        y: iy,
+    }, {
+        x: fx,
+        y: fy,
+        duration: random(5, 15),
+        // ease: "slow (0.7, 0.7, false)",
+        ease: "rough ({template: none.out, strength: 1, points: 20, taper: 'none', randomize: true, clamp:false})",
+        repeat: -1,
+        yoyo: true,
+    });
+    gsap.fromTo(fireflies[i], {
+        boxShadow: "0 0 0 0px rgba(154, 205, 50, 0)"
+    }, {
+        boxShadow: "0 0 0 3px rgba(154, 205, 50, 0.4)",
+        duration: random(2, 4),
+        ease: "slow (0.7, 0.7, false)",
+        repeat: -1,
+        yoyo: true,
+    });
+    gsap.fromTo(fireflies[i], {
+        opacity: 0,
+    }, {
+        duration: random(1, 5),
+        opacity: 0.9,
+        repeat: -1,
+        yoyo: true,
+    })
+};
